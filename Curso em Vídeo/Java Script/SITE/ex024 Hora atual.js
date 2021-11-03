19fd4676235569cedb1dca1000b4738c0ee6313d
Matheus França <89233100+franssa01@users.noreply.github.com>
2021-11-03 14:12:59 -0300

    Add files via upload

---
+++ Curso em Vídeo/Java Script/SITE/ex024 Hora atual.js
@@ -0,0 +1,12 @@
+let agora = new Date()
+let hora = agora.getHours()
+console.log(`Agora são ${hora} horas`)
+    if (hora < 12) {
+        console.log("Buenos dias cucaratcha")
+        } else if(hora < 18) {
+            console.log("Buenas tardes cucaratcha")
+            } else if (hora <= 24) {
+                console.log("Vai dormir")    
+                } 
+        
+    
