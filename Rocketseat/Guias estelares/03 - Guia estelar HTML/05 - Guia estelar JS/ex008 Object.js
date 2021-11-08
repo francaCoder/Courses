cc41e2d0653eb9fc5de636e5ab217dc124b2a980
Matheus França <89233100+franssa01@users.noreply.github.com>
2021-11-08 18:51:29 -0300

    Add files via upload

---
+++ Rocketseat/Guias estelares/03 - Guia estelar HTML/05 - Guia estelar JS/ex008 Object.js
@@ -0,0 +1,15 @@
+/*
+Tipo de dado que cria uma estrutura
+Vão possuir propriedades (Peso, altura, etc) / Atributos
+funcionalidades / Métodos
+
+{ Propriedade : "Valor" }
+*/
+
+console.log({
+    name: "França",
+    idade: 18,
+    andar: function() {
+        console.log("Andar")
+    }
+})
