02301f25d22e907775f12758fc9846daa669bbbd
Matheus França <89233100+franssa01@users.noreply.github.com>
2021-11-09 19:16:38 -0300

    Delete Rocketseat/Guias estelares/03 - Guia estelar HTML/05 - Guia estelar JS/05 - Guia estelar JS directory

--- Rocketseat/Guias estelares/03 - Guia estelar HTML/05 - Guia estelar JS/05 - Guia estelar JS/ex028 Length.js
+++
@@ -1,6 +0,0 @@
-let word = "Paralelepípedo" 
-console.log(word.length) // Vai mostrar quantos caracteres tem na palavra que word receber
-
-let number = 321
- // console.log(number.length) number não recebe length justamente por ele ser number, faça a conversão
- console.log(String(number).length)
--- Rocketseat/Guias estelares/03 - Guia estelar HTML/05 - Guia estelar JS/05 - Guia estelar JS/ex041 Grouping operator.js
+++
@@ -1,4 +0,0 @@
-// Usando o gruoping operator eu posso trocar a ordem de precedência da operação
-
-let total = (2 + 3) * 5
-console.log(total)
