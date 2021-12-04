510bb5206fb4e6d692be050ba1701e19603e2d3b
Matheus França <89233100+franssa01@users.noreply.github.com>
2021-12-04 18:40:56 -0300

    Delete Rocketseat/Guias estelares/03 - Guia estelar HTML/05 - Guia estelar JS directory

--- Rocketseat/Guias estelares/03 - Guia estelar HTML/05 - Guia estelar JS/ex005 Number.js
+++
@@ -1,6 +0,0 @@
-/* 
-33 / Inteiros
-12.5 / Reais - Float
-NaN / Not a number
-Infinity / Infinito
-*/
--- Rocketseat/Guias estelares/03 - Guia estelar HTML/05 - Guia estelar JS/ex022 Arrow function.js
+++
@@ -1,7 +0,0 @@
-// Arrow function
-
-const sayMyName = (name) => {
-    console.log(name)
-}
-
-sayMyName("Matheus França") // Não esquece do argumento 
