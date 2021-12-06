e071d57aa02568a2224fe21c0dc87c7ca7fb1360
Matheus França <89233100+franssa01@users.noreply.github.com>
2021-12-06 19:58:15 -0300

    Add files via upload

---
+++ Udemy/JavaScript Ninja/Ciclo 016/01 use strict.js
@@ -0,0 +1,14 @@
+/* 
+You can use 'use strict' inside any block or at the beginning of your code
+*/
+
+'use strict'
+
+(function(){
+    'use strict'
+var name = "Matheus"
+console.log(name)
+})()
+
+console.log(name)
+    
