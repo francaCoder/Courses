13d14af38648759f15b4b49a14dd39f2332c5161
Matheus França <89233100+franssa01@users.noreply.github.com>
2021-12-14 00:01:02 -0300

    Add files via upload

---
+++ Udemy/JavaScript Ninja/Ciclo 018/01 Regex.js
@@ -0,0 +1,30 @@
+// Regular expressions serves for manipulate Strings
+
+/*   
+\s = Spaces
+\n = Breaks line
+\t = Tab
+ . = All characters, but that break line
+*/
+
+// repeaters
+/*
+ .....   one point for each character
+ \w\w\w
+\d\d\d
+ */
+
+
+// Denail
+
+/* 
+[^abc] - The match can be done with any characters, but these
+\W (Capital) - All characters that don't are Alpha numerics
+\S (Capital) - All character that don't are space
+*/
+
+/* 
+"a"{2 = min, 4 = max} how much characters
+? Optional
++ One or more character
+*/
