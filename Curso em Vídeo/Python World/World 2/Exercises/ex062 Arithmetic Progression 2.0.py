c8b15d35fafd0638e2d60b8a8dcf2cf8e006b3e7
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-01-09 12:14:22 -0300

    Update ex062 Arithmetic Progression 2.0.py

--- Curso em Vídeo/Python World/World 2/Exercises/ex062 Arithmetic Progression 2.0.py
+++ Curso em Vídeo/Python World/World 2/Exercises/ex062 Arithmetic Progression 2.0.py
@@ -4,4 +4,10 @@ Redo the 'AP exercise' using the while
 
 firstAP = int(input("First term: "))
 reason = int(input("Reason: "))
-ten = firstAP + (10 - 1) * reason
+term = firstAP
+counter = 1
+while counter <= 10:
+    print("{} → ".format(term), end=" ")
+    term += reason
+    counter += 1
+print("End")
