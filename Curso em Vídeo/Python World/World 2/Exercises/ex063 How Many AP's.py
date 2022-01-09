bcbda966cba87a4ac4e9606b4634567dda405f7c
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-01-09 12:14:50 -0300

    Update ex063 How Many AP's.py

--- Curso em Vídeo/Python World/World 2/Exercises/ex063 How Many AP's.py
+++ Curso em Vídeo/Python World/World 2/Exercises/ex063 How Many AP's.py
@@ -1,1 +1,18 @@
 # Improve the previous exercise but now asking how many terms the user still wants to see
+
+firstAP = int(input("First term: "))
+reason = int(input("Reason: "))
+term = firstAP
+counter = 1
+total = 0
+more = 10
+while more != 0:
+    total += more
+    while counter <= total:
+        print("{} →".format(term), end=" ")
+        term += reason
+        counter += 1
+    print("PAUSE")
+
+    more = int(input("How many AP's you still wants to see? "))
+print("AP finished with {} progressions.".format(total))
