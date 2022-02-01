02939ac7e8becdebcc5d2ad9a9ab91dc0a20dab0
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-02-01 20:20:39 -0300

    Add files via upload

---
+++ Curso em Vídeo/Python World/World 3/Classes/CL017 Modules and Packs.py
@@ -0,0 +1,21 @@
+"""
+When our code starts to get big, it'shortcut time that use modules.
+- Create other file.py and put all functions inside.
+- When I want to use any function, i need import
+Ex ==> import file.py
+Ex ==> from file.py import function_name
+
+When our code starts to too far big, it'shortcut time that use packages
+- When I have many functions in one module, the problem come back.
+- Then, I can do several packages
+Ex ==> commands {
+    Numbers: Functions...
+    Strings: Functions...
+    Colors: Functions...
+    etc.
+}
+
+- When I want to use any function according my project, I need import
+Ex ==>  import commands
+Ex ==> From commands import Numbers
+"""
