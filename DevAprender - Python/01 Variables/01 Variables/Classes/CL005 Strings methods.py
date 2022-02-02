997a7823b03353476393301b8d8740ebd99a52cd
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-02-02 20:57:32 -0300

    Delete DevAprender - Python/01 Variables/01 Variables directory

--- DevAprender - Python/01 Variables/01 Variables/Classes/CL005 Strings methods.py
+++
@@ -1,8 +0,0 @@
-course_name = "   Python   "
-print(course_name.upper()) # Capital letters
-print(course_name.lower()) # Tiny letters
-print(course_name.strip()) # remove unnecessary spaces
-print(course_name.lstrip()) # " from left
-print(course_name.rstrip()) # " from right
-print(course_name.find("on")) # which index is the "of"?   - Starts from index 0
-print(course_name.replace("Python", "DevAprender - Python"))
