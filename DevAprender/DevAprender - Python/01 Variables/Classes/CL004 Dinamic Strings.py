b4c3df50db04cc59d8a5b5c5ed44bba285188697
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-02-02 21:02:20 -0300

    Add files via upload

---
+++ DevAprender/DevAprender - Python/01 Variables/Classes/CL004 Dinamic Strings.py
@@ -0,0 +1,5 @@
+# If I change name or email, using f-strings I will haven't an error
+
+name = "Matheus"
+email = "Matheus@hotmail.com"
+print(f"Hello {name}, you registered the email {email}, is this information correct?")
