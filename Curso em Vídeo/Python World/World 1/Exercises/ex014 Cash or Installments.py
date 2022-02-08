6b10b18a4b8ef49f1ecc431375b9bfe9908ef910
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-02-08 01:11:18 -0300

    Add files via upload

--- Curso em Vídeo/Python World/World 1/Exercises/ex014 Cash or Installments.py
+++ Curso em Vídeo/Python World/World 1/Exercises/ex014 Cash or Installments.py
@@ -9,7 +9,7 @@ newIncrease = (increase * 8 / 100)
 
 print("Payment in cash with 10% Discount: {}".format(discount))
 print(" ")
-print("For payments in installments, have a increase of 8% p/m. This same product would cost:")
+print("For payments in installments, have an increase of 8% p/m. This same product would cost:")
 print("First month: {:.2f}".format(increase))
 print("Second month: {:.2f}".format(increase + newIncrease))
 print("Third month: {:.2f}".format(increase + newIncrease * 2))
@@ -17,4 +17,23 @@ print("Fourth month: {:.2f}".format(increase + newIncrease * 3))
 print("Fifth month: {:.2f}".format(increase + newIncrease * 4))
 print("Sixth month: {:.2f}".format(increase + newIncrease * 5))
 print("Sixth month: {:.2f}".format(increase + newIncrease * 6))
+
+
+# OR
+
+price = float(input("Type the product's price: R$"))
+promotion = price * 10 / 100
+discount = price - promotion
+increase = price + (price * 8 / 100)
+newIncrease = (increase * 8 / 100)
+
+print(f"Payment in cash with 10% Discount: {discount}")
+print()
+print("For payments in installments, have an increase of 8% p/m. This same product would cost:")
+for c in range(0, 6):
+    if c == 0:
+        print(f"{c+1}º Month: {increase:.2f}")
+    elif c >= 1:
+        print(f"{c+1}º Month: {increase + newIncrease * c:.2f}")
+
 
