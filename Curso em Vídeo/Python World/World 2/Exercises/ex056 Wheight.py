792c525e29a6af4af8b182ac570a2bbeada3d9bb
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-02-12 23:18:11 -0300

    Update ex056 Wheight.py

--- Curso em Vídeo/Python World/World 2/Exercises/ex056 Wheight.py
+++ Curso em Vídeo/Python World/World 2/Exercises/ex056 Wheight.py
@@ -1,34 +1,36 @@
 # Show the biggest and the smallest wheight between 5 peoples
 
-# biggest = smallest = 0
-# for person in range(1, 6):
-#     weight = float(input("The weight of {}ª person: ".format(person)))
-#     if person == 1:
-#         biggest = weight
-#         smallest = weight
-#     elif weight > biggest:
-#         biggest = weight
-#     elif weight < smallest:
-#         smallest = weight
-#
-#
-#
-# print("The biggest weight read was {}Kg.".format(biggest))
-# print("The smallest weight read was {}Kg.".format(smallest))
+biggest = smallest = 0
+for person in range(1, 6):
+    weight = float(input("The weight of {}ª person: ".format(person)))
+    if person == 1:
+        biggest = weight
+        smallest = weight
+    elif weight > biggest:
+        biggest = weight
+    elif weight < smallest:
+        smallest = weight
+
+
+
+print("The biggest weight read was {}Kg.".format(biggest))
+print("The smallest weight read was {}Kg.".format(smallest))
 
 # Or
 
-# def people(num=0):
-#     weights = []
-#     for c in range(0, num):
-#         weights.append(float(input(f"{c+1}º Person's Weight: ")))
-#     print(f"The biggest weight read was {max(weights)}Kg.")
-#     print(f"The smallest weight read was {min(weights)}Kg.")
-#
-#
-# people(5)
+def people(num=0):
+    weights = []
+    for c in range(0, num):
+        weights.append(float(input(f"{c+1}º Person's Weight: ")))
+    print(f"The biggest weight read was {max(weights)}Kg.")
+    print(f"The smallest weight read was {min(weights)}Kg.")
+
+    
+people(5)
 
 # Or
+
+
 def how_many_people(num=0):
     people = []
     for c in range(0, num):
