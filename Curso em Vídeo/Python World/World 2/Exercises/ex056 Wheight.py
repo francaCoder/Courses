45c8997aadae1f15eab72ad40013d94f5c68f297
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-02-13 21:10:08 -0300

    Update ex056 Wheight.py

--- Curso em Vídeo/Python World/World 2/Exercises/ex056 Wheight.py
+++ Curso em Vídeo/Python World/World 2/Exercises/ex056 Wheight.py
@@ -25,38 +25,53 @@ def people(num=0):
     print(f"The biggest weight read was {max(weights)}Kg.")
     print(f"The smallest weight read was {min(weights)}Kg.")
 
-    
+
 people(5)
 
 # Or
-
-
 def how_many_people(num=0):
     people = []
     for c in range(0, num):
+        print(f"{c+1}º Person")
         person = {
             "Name": str(input("Name: ")),
             "Weight": float(input("Weight: "))
         }
+
         people.append(person)
 
-    print(people)
-
     weights = []
+    over_weight = []
+    under_weight = []
+
     for c in range(0, len(people)):
         weights.append(people[c]['Weight'])
 
     greater_weight = max(weights)
     less_weight = min(weights)
 
-    print(f"The greater weight was {greater_weight}")
     for c in range(0, len(people)):
         if greater_weight == people[c]['Weight']:
-            print(f"{people[c]['Name']} weighs → {greater_weight}")
+            over_weight.append(people[c]['Name'])
+
+        if less_weight == people[c]['Weight']:
+            under_weight.append(people[c]['Name'])
+
+    print()
+    print("-=-=-=-= Result =-=-=-=-")
+    print(f"The greater weight was << {greater_weight} >>")
+    for c in range(0, len(over_weight)):
+        print(f"{over_weight[c]}", end="")
+        print(", " if c < len(over_weight) - 1 else " → ", end="")
+    print(f"Weigh {greater_weight}")
 
     print()
 
-    print(f"The less weight was {less_weight}")
+    print(f"The less weight was << {less_weight} >>")
+    for c in range(0, len(under_weight)):
+        print(f"{under_weight[c]}", end="")
+        print(", " if c < len(under_weight) - 1 else " → ", end="")
+    print(f"Weigh {less_weight}")
 
 
-how_many_people(3)
+how_many_people(4)
