6488a7d4894cc6d66e15828f3985f2db2036ca83
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-03-02 20:00:54 -0300

    Add files via upload

---
+++ DevAprender/Mestre da Automação/04 Mestre do Teclado/Classes/CL031 PyAutoGui.py
@@ -0,0 +1,14 @@
+import pyautogui
+import mouseinfo
+
+"""
+>>> import pyautogui
+>>> pyautogui.size()
+Size(width=1920, height=1080)
+>>> pyautogui.displayMousePosition()
+Press Ctrl-C to quit.
+X:  509 Y:  986 RGB: ( 28,  30,  38)
+>>> 
+>>> import mouseinfo 
+>>> mouseinfo.mouseInfo()
+"""
---
+++ DevAprender/Mestre da Automação/04 Mestre do Teclado/Classes/CL032 Automatically Moving.py
@@ -0,0 +1,10 @@
+from time import sleep
+
+import pyautogui
+import mouseinfo
+
+print(pyautogui.position())
+
+
+pyautogui.moveTo(x=514, y=1059, duration=2)
+pyautogui.click()
