423cbea218b1eb932d65f7fdfa63f517bb02aea3
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-05-25 16:35:02 -0300

    Add files via upload

--- Hashtag Programação/Python Impressionador/Projeto 1 - Automação/Automação de Indicadores/code.py
+++ Hashtag Programação/Python Impressionador/Projeto 1 - Automação/Automação de Indicadores/code.py
@@ -24,40 +24,94 @@ quais indicadores aquela loja conseguiu cumprir naquele dia ou não.
 # 5 - Enviar o OnePage
 # 6 - Enviar o E-mail para a diretoria
 
+import win32com.client as win32
 import pandas as pd
 import pathlib
 
-sales = pd.read_excel(r"Bases de Dados\Vendas.xlsx")
+vendas = pd.read_excel(r"Bases de Dados\Vendas.xlsx")
 emails = pd.read_excel(r"Bases de Dados\Emails.xlsx")
-stores = pd.read_csv(r"Bases de Dados\Lojas.csv", encoding='latin1', sep=';')
+lojas = pd.read_csv(r"Bases de Dados\Lojas.csv", encoding='latin1', sep=';')
 
 # print(sales.info())
 # print(emails.info())
 # print(stores.info())
 
-sales = sales.merge(stores, on='ID Loja')
+vendas = vendas.merge(lojas, on='ID Loja')
 # print(sales[['ID Loja', 'Loja']])
 
-dic_stores = {}
-for store in stores['Loja']:
-    dic_stores[store] = sales.loc[sales['Loja'] == store, :]
+dic_lojas = {}
+for loja in lojas['Loja']:
+    dic_lojas[loja] = vendas.loc[vendas['Loja'] == loja, :]
 
-dia_indicador = sales['Data'].max()
-# Salvando os Backups
-backup_path = pathlib.Path(r"Backup Arquivos Lojas")
-files_backup_folder = backup_path.iterdir()
+dia_indicador = vendas['Data'].max()
 
-list_stores_backup = [file.name for file in files_backup_folder]
+# Salvando os backups
+caminho_backup = pathlib.Path(r"Backup Arquivos Lojas")
+files_backup_folder = caminho_backup.iterdir()
 
-print(list_stores_backup)
+lista_lojas_backup = [arquivo.name for arquivo in files_backup_folder]
 
-for store in dic_stores:
-    if store not in list_stores_backup:
-        new_folder = backup_path / store
-        new_folder.mkdir()
+for loja in dic_lojas:
+    if loja not in lista_lojas_backup:
+        nova_pasta = caminho_backup / loja
+        nova_pasta.mkdir()
 
-    file_name = f"{dia_indicador.day}_{dia_indicador.month}_{store}.xlsx"
-    print(file_name)
-    file_path = backup_path / store / file_name
+    nome_arquivo = f"{dia_indicador.day}_{dia_indicador.month}_{loja}.xlsx"
+    caminho_arquivo = caminho_backup / loja / nome_arquivo
 
-    dic_stores[store].to_excel(file_path)
+    dic_lojas[loja].to_excel(caminho_arquivo)
+
+# Indicadores e Faturamento
+
+loja = "Norte Shopping"
+vendas_loja = dic_lojas[loja]
+
+faturamento_anual = vendas_loja['Valor Final'].sum()
+print(faturamento_anual)
+
+vendas_diarias_loja = vendas_loja.loc[vendas_loja['Data'] == dia_indicador, :]
+faturamento_diario = vendas_diarias_loja['Valor Final'].sum()
+print(faturamento_diario)
+
+# Diversidade de produtos
+
+quantidade_anual_produtos = vendas_loja['Produto'].unique()
+print(len(quantidade_anual_produtos))
+quantidade_diaria_produtos = vendas_diarias_loja['Produto'].unique()
+print(len(quantidade_diaria_produtos))
+
+valor_vendas = vendas_loja.groupby("Código Venda").sum()
+ticket_medio_anual = valor_vendas['Valor Final'].mean()
+print(ticket_medio_anual)
+
+valor_vendas_diarias = vendas_diarias_loja.groupby("Código Venda").sum()
+ticket_medio_diario = valor_vendas_diarias['Valor Final'].mean()
+print(ticket_medio_diario)
+
+meta_faturamento_dia = 1000
+meta_faturamento_ano = 1650000
+meta_quantidade_produtos_dia = 4
+meta_quantidade_produtos_ano = 120
+meta_ticketmedio_dia = 500
+meta_ticketmedio_ano = 500
+
+outlook = win32.Dispatch("outlook.application")
+
+nome = emails.loc[emails['Loja'] == loja, 'Gerente'].values[0]
+
+email = outlook.CreateItem(0)
+email.to = "matheusailvafds@gmail.com"
+# mail.CC = cópia
+# mail.BCC = cópia oculta
+email.Subject = f"OnePage Dia {dia_indicador.day}/{dia_indicador.month} - loja {loja}"
+email.HTMLBody = f"""
+<p>{emails.loc[emails["Loja"]==loja, "E-mail"].values[0]}</p>
+Bom Dia, {nome}
+
+Texto do E-mail
+"""
+
+attachment = pathlib.Path.cwd() / caminho_backup / loja / f"{dia_indicador.day}_{dia_indicador.month}_{loja}.xlsx"
+email.Attachments.Add(str(attachment))
+
+email.Send()
