dad6ce951445677913452360fcaf374a84345db2
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-06-12 10:32:10 -0300

    Add files via upload

---
+++ Hashtag Programação/Python Impressionador/18 Integração SQL/Exercises/como salvar tabelas.sql
@@ -0,0 +1,5 @@
+-- Para criar uma nova tabela/banco de dados, basta colocar antes do SELECT
+-- Create View nome_da_tabela as
+-- e embaixo o código normal
+
+-- E aí depois de todas as tabelas manipuladas, eu posso puxar lá no power BI
