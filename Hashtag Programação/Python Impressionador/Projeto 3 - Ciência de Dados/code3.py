94289ff4e547304e0d5d98c8189266c21a8155a7
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-06-15 23:50:24 -0300

    Add files via upload

--- Hashtag Programação/Python Impressionador/Projeto 3 - Ciência de Dados/code3.py
+++ Hashtag Programação/Python Impressionador/Projeto 3 - Ciência de Dados/code3.py
@@ -3,6 +3,10 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
+from sklearn.model_selection import train_test_split
 
 base_airbnb = pd.read_csv("base_airbnb.csv", sep=',')
 
@@ -25,7 +29,7 @@ base_airbnb['extra_people'] = base_airbnb['extra_people'].str.replace("$", "")
 base_airbnb['extra_people'] = base_airbnb['extra_people'].str.replace(",", "")
 base_airbnb['extra_people'] = base_airbnb['extra_people'].astype(np.float64, copy=False)
 
-print(base_airbnb.info())
+# print(base_airbnb.info())
 # print(base_airbnb.iloc[0])
 
 # começar a análise exploratória
@@ -82,7 +86,7 @@ def diagrama_caixa(coluna):
     plt.show()
 
 
-print(diagrama_caixa(base_airbnb['price']))
+# print(diagrama_caixa(base_airbnb['price']))
 
 
 def histograma(coluna):
@@ -364,12 +368,12 @@ base_airbnb = base_airbnb.drop("amenities", axis=1)
 
 # Agora que pegamos o len da coluna de amenities, ela virou uma coluna numérica, então nós podemos tratar ela igual tratamos outras colunas e também excluir os outliers
 
-print(diagrama_caixa(base_airbnb['n_amenities']))
-print(grafico_barra(base_airbnb['n_amenities']))
+# print(diagrama_caixa(base_airbnb['n_amenities']))
+# print(grafico_barra(base_airbnb['n_amenities']))
 
 base_airbnb, linhas_removidas = excluir_outliers(base_airbnb, 'n_amenities')
-print(f"{linhas_removidas} linhas removidas na coluna de n_amenities")
-print(histograma(base_airbnb['n_amenities']))
+# print(f"{linhas_removidas} linhas removidas na coluna de n_amenities")
+# print(histograma(base_airbnb['n_amenities']))
 
 
 # Vizualização de mapa das propriedades, latitude e longitude
@@ -383,3 +387,165 @@ centro_mapa = {"lat": amostra.latitude.mean(), "lon": amostra.longitude.mean()}
 mapa = px.density_mapbox(amostra, lat="latitude", lon="longitude", z="price", radius=2.5, center=centro_mapa, zoom=10, mapbox_style="stamen-terrain")
 
 mapa.show()
+
+# Encoding
+# Precisamos ajustar as features para facilitar o trabalho do modelo futuro (features de categoria, true e false, etc.)
+# True or False = T:1 F:0
+# para features de categoria vamos utilizar o método de encoding de variáveis dummies
+
+colunas_tf = ["host_is_superhost", "instant_bookable", "is_business_travel_ready"]
+colunas_categorias = ["property_type", "room_type", "bed_type", "cancellation_policy"]
+
+# Codificação das colunas t/f
+base_airbnb_cod = base_airbnb.copy()
+for column in base_airbnb:
+    base_airbnb_cod.loc[base_airbnb_cod[column] == "t", column] = 1
+    base_airbnb_cod.loc[base_airbnb_cod[column] == "f", column] = 0
+
+
+# Codificação das colunas de categorias (usando dummies)
+base_airbnb_cod = pd.get_dummies(data=base_airbnb_cod, columns=colunas_categorias)
+# pd.get_dummies te dá como resposta um DataFrame que vai transformar algumas colunas em dummies
+
+
+# Tudo que fizemos até agora foi preparar a nossa base de dados para aplicar nossos modelos nela
+
+
+### Modelo de previsão
+# Usando a biblioteca Sklearn - Biblioteca de inteligência artificial
+# R² e o Erro quadrático médio
+
+def avaliar_modelo(nome_modelo, y_teste, previsao):
+    """
+    :param nome_modelo: Só para poder printar o nome do nosso modelo
+    :param y_teste: Valores reais/Corretos que vão ser usados para testar nosso modelo
+    :param previsao: Vamos ver se a previsão está perto ou não do valor correto e depois julgar se o modelo é bom ou ruim
+    :return:
+    """
+
+    # Métricas de avaliação
+    r2 = r2_score(y_teste, previsao)
+    RSME = np.sqrt(mean_squared_error(y_teste, previsao))
+    # Vai tirar a raiz quadrada do mean squared error
+    return f"Modelo: {nome_modelo}\nR²: {r2}\nRSME: {RSME}"
+
+    # Pegando as previsões e comparando com os testes seguindo duas métricas
+
+
+# Escolha dos modelos a serem testados
+# Esses modelos já foram criados para gente (todos na mesma biblioteca do sklearn), só precisamos entender como funciona e como se aplica, e não conhecer os mínimos detalhes de cada um (pelo menos inicialmente).
+# 1 - Random Forest
+# 2 - Linear Regression
+# 3 - Extra trees
+
+modelo_rf = RandomForestRegressor()
+modelo_lr = LinearRegression()
+modelo_et = ExtraTreesRegressor()
+
+modelos = {
+    "RandomForest": modelo_rf,
+    "LinearRegression": modelo_lr,
+    "ExtraTrees": modelo_et
+}
+
+# Separação das variáveis
+y = base_airbnb_cod['price']
+x = base_airbnb_cod.drop('price', axis=1)
+
+# Separar os dados em treino e teste + Treino do modelo
+# Tenho que fazer essa separação tanto para as colunas quanto para o preço que é o que eu quero calcular
+
+# ""Olha as característica desses 8 imóveis aqui, agora, com base nesses dados que você viu, precifica para mim esses novos dois imóveis com base no seu conhecimento""
+
+# E também já temos um modelo pronto de separação dentro do sklearn
+# train_test_split = Vai fazer um split em dados de treino e em dados de teste
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=10)
+# Random_state _ estou determinando ele à usar sempre como semente o número 10, a vantagem é de que se eu rodar os códigos outras vezes, ele vai fazer a mesma divisão na base de dados, e não vai ficar mudando os dados de teste / treino
+
+# Agora precisamos treinar e testar cada um dos modelos
+for nome_modelo, modelo in modelos.items():
+    # Treinar
+    modelo.fit(x_train, y_train)
+    # Testar
+    previsao = modelo.predict(x_test)
+    print(avaliar_modelo(nome_modelo, y_test, previsao))
+
+
+# Modelo: RandomForest
+# R²: 0.9855938815939891 - 98.56%
+# RSME: 31.79745982390543
+#
+# Modelo: LinearRegression
+# R²: 0.3268426558013201 - 32.68%
+# RSME: 217.35880184606643
+#
+# Modelo: ExtraTrees
+# R²: 0.9874667994490411 - 98.75%
+# RSME: 29.658551173359623
+
+# - Melhor modelo: # Modelo: ExtraTrees
+# Esse foi o modelo com maior valor de R² e ao mesmo tempo o menor valor de RSME
+# Não tivemos grande diferença de velocidade entre ele e o segundo colocado
+# O modelo de regressão linear não teve um resultado satisfatório, muito pior do que os outros modelos
+
+
+# Agora que a gente já escolheu o nosso melhor modelo, agora vamos entender como ele tá funcionando e ver possíveis melhorias
+
+# Nossos modelos tem uma função que exibe a importância de cada feature, que ele levou em consideração na sua análise. Assim podemos ver as colunas mais importantes e também as colunas que ele não utiliza ou que quase não leva em consideração, e assim nós podemos remover elas e melhorar ainda mais o nosso modelo.
+
+importancia_features = pd.DataFrame(modelo_et.feature_importances_, x_train.columns)
+importancia_features.sort_values(by=0, ascending=True, inplace=True)
+
+plt.figure(figsize=(15, 5))
+ax = sns.barplot(x=importancia_features.index, y=importancia_features[0])
+ax.tick_params(axis="x", rotation=90)
+
+
+# Latitude e longitude de fato é realmente muito importante e já era de se imaginar que ía ficar em primeiro. devido a localização que é próximo as praias, próximo aos pontos turísticos, locais mais seguros, faz sentido o imóvel ser mais caro
+
+# quantidade de quartos
+
+# amenities surpreendendo. Pode ser que ,no caso dos amenities que detalharam mais as casas realmente tenham mais coisas ou que os amenities que tem menos é porque o host não entra tanto a fundo no anúncio e não consegue cobrar um preço mais alto pelo seu imóvel
+
+# Detalhar mais um pouco sobre as outras colunas....
+
+# Ajustes finais
+# A colunas is_business_travel_ready não tem nenhum grau de importância no resultado da função de importância de features, por isso vamos tirar ela da nossa base de dados e treinar o nosso modelo novamente para ver se as nossas métricas melhoram ou não
+
+base_airbnb_cod = base_airbnb_cod.drop("is_business_travel_ready", axis=1)
+
+y = base_airbnb_cod['price']
+x = base_airbnb_cod.drop('price', axis=1)
+
+# Tirando as colunas de cama (mais para teste mesmo e porque ela tiveram uma importância baixa)
+
+base_teste = base_airbnb_cod.copy()
+for coluna in base_teste:
+    if "bed_type" in coluna:
+        base_teste = base_teste.drop(coluna, axis=1)
+
+y = base_teste['price']
+x = base_teste.drop("price", axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=10)
+
+modelo_et.fit(x_train, y_train)
+previsao = modelo_et.predict(x_test)
+print(avaliar_modelo('ExtraTrees', y_test, previsao))
+
+
+### Deploy do projeto
+# 1 - Criar um arquivo do nosso modelo já treinado usando i picle da biblioteca joblib
+# 2 - Escolher a forma de deploy:
+#     - Arquivo exe + Tkinter
+#     - Deploy em um microsite (Flask)
+#     - Deploy para uso direto (Streamlit)
+
+
+
+x['price'] = y
+x.to_cvs("dados.csv")
+
+import joblib
+joblib.dump(modelo_et, "modelo.joblib") # é um arquivo pesado mas dentro dele já está o modelo pronto
