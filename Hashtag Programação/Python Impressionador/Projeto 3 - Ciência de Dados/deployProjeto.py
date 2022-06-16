c8721cb75ae8062da80c4d876aac9df9563ca4a8
Matheus França <89233100+franssa01@users.noreply.github.com>
2022-06-16 00:00:04 -0300

    Add files via upload

---
+++ Hashtag Programação/Python Impressionador/Projeto 3 - Ciência de Dados/deployProjeto.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import streamlit as st
+import joblib
+
+# Preciso perguntar para o usuário todas as características necessárias que o nosso modelo leva em conta para descobrir o preço do imóvel
+
+modelo = joblib.load("modelo.joblib")
+
+# colunas da tabela
+
+
+x_numericos = {'latitude': 0, 'longitude': 0, 'accommodates': 0, 'bathrooms': 0, 'bedrooms': 0, 'beds': 0, 'extra_people': 0,
+               'minimum_nights': 0, 'ano': 0, 'mes': 0, 'n_amenities': 0, 'host_listings_count': 0}
+
+x_tf = {'host_is_superhost': 0, 'instant_bookable': 0}
+
+x_listas = {'property_type': ['Apartment', 'Bed and breakfast', 'Condominium', 'Guest suite', 'Guesthouse', 'Hostel', 'House', 'Loft', 'Outros', 'Serviced apartment'],
+            'room_type': ['Entire home/apt', 'Hotel room', 'Private room', 'Shared room'],
+            'cancelation_policy': ['flexible', 'moderate', 'strict', 'strict_14_with_grace_period']
+            }
+
